4ac26f2ecba24f893b287d1f491afaee1df1a32e
ankith1207cvr <123532761+ankith1207cvr@users.noreply.github.com>
2023-02-08 11:43:33 +0530

    Add files via upload

---
+++ destr.js
@@ -0,0 +1,16 @@
+const user={
+    firstName:"Ankith",
+    lastName:"sai",
+    address:{
+        city:"haliya",
+        district:"nalgonda"
+    }
+}
+console.log(user.firstName+user.lastName);
+console.log(user.address.city);
+
+//2.
+// const{firstName,lastName,address:{city}}=user;
+// console.log(firstName+lastName+city);
+
+
